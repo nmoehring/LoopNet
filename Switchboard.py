f38fd859bb802d9cdfd3fa74ebf51f6b911f5c0c
Nathaniel Moehring <nathan.moehring@gmail.com>
2024-06-24 13:44:12 -0500

    Throwing a bunch of random crap into a readme.

--- Switchboard.py
+++ Switchboard.py
@@ -5,35 +5,6 @@ from Node import Node, NodeType
 from lingo import NT, LM, NS, NodeState, LT, LD
 
 
-# Loops are connected nodes, nodes send data one way (need to figure out how to prevent
-#   data going the wrong direction on sockets)
-# Knots are 2 loops tied together, one going the opposite direction
-#   4 Loops are tied together into 4 knots, so each node belongs to 2 knots
-# The complete 4 loops in 4 knots is a toroid, and I guess will be considered the most
-#   fundamental stable unit of the LoopNet
-# There are many paths through the toroid, so the toroid may be the most capable
-#   small-scale structure to handle loop breakages or other issues, or maybe just
-#   the largest identifiable structure
-
-# The most complete toroid possible forms from the beginning
-# The first 4 nodes should be in separate loops, laced together
-# Once stack is established, stacks split
-# Stack-stacks form, with different node columns representing at different layers
-#
-
-# 0 - LOOPBACK
-# 1 - Connection Loop And Stack - Connect to everything everywhere as good as possible
-#                               - Higher
-
-# Connecting node:
-#      Accept
-# Insertion node sends message forward:
-#      "expect new connection from the new node address"
-# Insertion
-# New node connects to their new output node
-# New node sends message through new loop which contains their address
-# Mail travels around to
-
 class Switchboard:
     def __init__(self, taskgroup):
         self.server = None
@@ -107,11 +78,6 @@ class Switchboard:
 
     def accept_connection(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         info = MetaLink.from_streams(reader, writer, self)
-        #Need to know what node to connect to
-        #connect() just suppposed to advertise intent to connect
-        #a new node is inserted when the loop node connects to the server of the new node,
-        # then the oNode assignment for the new node and the iNode assignment for the next
-        # node in the loop are directed by the insertion node
         self.connectBuffer.append(info)
         print(info.clientAddr, ":", info.clientPort, "connected.")
             #print(sock.getsockopt('peername'))
